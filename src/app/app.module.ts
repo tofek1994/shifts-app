c1d050d438df38676077f2bbf23a5db62cef178c
Tofek Salameh <tofek1994s@gmail.com>
2019-08-07 21:56:40 +0300

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
+    LoginComponent,
     AppComponent
   ],
   imports: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'
+const routes: Routes = [
+{path : '', component: LoginComponent, pathMatch: 'full'},
+ // {path: '**', redirectTo: '' }
 
-
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
